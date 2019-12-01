62b4b2654987c7ea8ea56c0aec07f6d1c4aeafd5
Benoit Katecpo Izakov <scaniagaming@live.fr>
2019-12-01 13:58:53 +0100

    Projet de base

---
+++ SpeculPlus/SpeculPlus/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace SpeculPlus
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var scanPage = new ZXingScannerPage();
+            scanPage.DefaultOverlayTopText = "Alignez le code-barre";
+            // Navigation au scanner
+            await Navigation.PushAsync(scanPage);
+
+            scanPage.OnScanResult += (result) =>
+            {
+                // Stop scanning
+                scanPage.IsScanning = false;
+
+                // Pop the page and show the result
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopAsync();
+                    await DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+        }
+    }
+}
---
+++ SpeculPlus/SpeculPlus/ScanPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
+
+namespace SpeculPlus
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ScanPage : ContentPage
+	{
+        ZXingScannerView zxing;
+
+        public ScanPage() : base()
+		{
+            //initialisation de la View ZsingScanner
+            zxing = new ZXingScannerView
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            // Dès que le capteur trouve un Qr code, code barre, ...
+            zxing.OnScanResult += (result) =>
+                Device.BeginInvokeOnMainThread(async () => {
+                    // Stop l'analyse 
+                    zxing.IsAnalyzing = false;
+                    // Ouvre une Pop-up affichant le texte, lien ...
+                    await DisplayAlert("Résultat", result.Text, "OK");
+                    await Navigation.PopAsync();
+                });
+
+            //Création de la Grid
+            var grid = new Grid
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+            //Ajout du composant dans la Grid
+            grid.Children.Add(zxing);
+            Content = grid;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            // Scan
+            zxing.IsScanning = true;
+        }
+
+        protected override void OnDisappearing()
+        {
+            // Ne scan plus
+            zxing.IsScanning = false;
+            base.OnDisappearing();
+        }
+    }
+}
